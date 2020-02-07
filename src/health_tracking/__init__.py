62f1d98c657dba25260dc8caae96d0dfb12c6ef9
Sebastian Jäger <message@sebastian-jaeger.me>
2020-02-07 10:17:47 +0100

    performance improvements by storing extractions and some visibility changes

--- src/health_tracking/__init__.py
+++ src/health_tracking/__init__.py
@@ -56,8 +56,18 @@ class AppleHealthParser(object):
                 zipped_export = zipfile.ZipFile(zip_file_bytes)
                 zipped_export.extractall(os.path.split(unzip_path)[0])  # need path to dir not file
 
-        self.tree = ET.parse(constants.XML_PATH)
-        self.health_data = self.tree.getroot()
+        self._tree = ET.parse(constants.XML_PATH)
+        self._health_data = self._tree.getroot()
+
+        # element types
+        self._export_date = None
+        self._me = None
+        self._workouts = None
+        self._workout_types = None
+        self._activity_summaries = None
+        self._records = None
+        self._correlations = None
+        self._clinical_records = None
 
     def _fix_data_types(self, data_frame: pd.DataFrame) -> pd.DataFrame:
         """
@@ -100,7 +110,7 @@ class AppleHealthParser(object):
         if element_type not in constants.ELEMENT_TAGS:
             raise ValueError(f"'element_type' need to be one of: {constants.ELEMENT_TAGS}")
 
-        elements = self.tree.findall(element_type)
+        elements = self._tree.findall(element_type)
         result = pd.DataFrame([element.attrib for element in elements])
 
         result = self._fix_data_types(result)
@@ -114,13 +124,18 @@ class AppleHealthParser(object):
         Returns:
             (pd.DataFrame, set): of type `Workout` or `None` if empty and set of available workout types
         """
-        data_frame = self._extract_elements_of_type(constants.WORKOUT_TAG)
-        data_frame[constants.WORKOUT_TYPE] = data_frame.apply(
-            lambda row: re.match(constants.WORKOUT_REGEX, row[constants.WORKOUT_TYPE]).group(1),
-            axis=1
-        )
 
-        return data_frame, set(data_frame[constants.WORKOUT_TYPE])
+        # increase performace by do not parse again.
+        if self._workouts is None and self._workout_types is None:
+
+            self._workouts = self._extract_elements_of_type(constants.WORKOUT_TAG)
+            self._workouts[constants.WORKOUT_TYPE] = self._workouts.apply(
+                lambda row: re.match(constants.WORKOUT_REGEX, row[constants.WORKOUT_TYPE]).group(1),
+                axis=1
+            )
+            self._workout_types = set(self._workouts[constants.WORKOUT_TYPE])
+
+        return self._workouts, self._workout_types
 
     def extract_me(self) -> pd.DataFrame:
         """
@@ -129,7 +144,12 @@ class AppleHealthParser(object):
         Returns:
             pd.DataFrame: of type `Me` or `None` if empty
         """
-        return self._extract_elements_of_type(constants.ME_TAG)
+
+        # increase performace by do not parse again.
+        if self._me is None:
+            self._me = self._extract_elements_of_type(constants.ME_TAG)
+
+        return self._me
 
     def extract_records(self) -> pd.DataFrame:
         """
@@ -138,7 +158,12 @@ class AppleHealthParser(object):
         Returns:
             pd.DataFrame: of type `Record` or `None` if empty
         """
-        return self._extract_elements_of_type(constants.RECORD_TAG)
+
+        # increase performace by do not parse again.
+        if self._records is None:
+            self._records = self._extract_elements_of_type(constants.RECORD_TAG)
+
+        return self._records
 
     def extract_correlations(self) -> pd.DataFrame:
         """
@@ -147,7 +172,12 @@ class AppleHealthParser(object):
         Returns:
             pd.DataFrame: of type `Correlation` or `None` if empty
         """
-        return self._extract_elements_of_type(constants.CORRELATION_TAG)
+
+        # increase performace by do not parse again.
+        if self._correlations is None:
+            self._correlations = self._extract_elements_of_type(constants.CORRELATION_TAG)
+
+        return self._correlations
 
     def extract_activity_summaries(self) -> pd.DataFrame:
         """
@@ -156,7 +186,12 @@ class AppleHealthParser(object):
         Returns:
             pd.DataFrame: of type `ActivitySummary` or `None` if empty
         """
-        return self._extract_elements_of_type(constants.ACTIVITY_SUMMARY_TAG)
+
+        # increase performace by do not parse again.
+        if self._activity_summaries is None:
+            self._activity_summaries = self._extract_elements_of_type(constants.ACTIVITY_SUMMARY_TAG)
+
+        return self._activity_summaries
 
     def extract_clinical_records(self) -> pd.DataFrame:
         """
@@ -165,7 +200,12 @@ class AppleHealthParser(object):
         Returns:
             pd.DataFrame: of type `ClinicalRecord` or `None` if empty
         """
-        return self._extract_elements_of_type(constants.CLINICAL_RECORD_TAG)
+
+        # increase performace by do not parse again.
+        if self._clinical_records is None:
+            self._clinical_records = self._extract_elements_of_type(constants.CLINICAL_RECORD_TAG)
+
+        return self._clinical_records
 
     def get_export_date(self) -> pd.Timestamp:
         """
@@ -174,5 +214,10 @@ class AppleHealthParser(object):
         Returns:
             pd.Timestamp: Export timestamp
         """
-        data_frame = self._extract_elements_of_type(constants.EXPORT_DATE_TAG)
-        return pd.to_datetime(data_frame["value"])[0]
+
+        # increase performace by do not parse again.
+        if self._export_date is None:
+            data_frame = self._extract_elements_of_type(constants.EXPORT_DATE_TAG)
+            self._export_date = pd.to_datetime(data_frame["value"])[0]
+
+        return self._export_date
